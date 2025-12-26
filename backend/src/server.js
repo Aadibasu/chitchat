9dd9cb215aea08f2bdc32dc6c6b8dce8b0c32412
aditya_basu123 <adityabasu6204@gmail.com>
2025-12-26 18:51:03 +0530

    ready for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,28 @@
 //const express = require('express');
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"; 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+//make ready for the deployment
+// making backend and frontend in a single url
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/vite-project/dist")))
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/vite-project/dist/index.html"))
+  })
+}
 
 app.listen(PORT,() =>
   console.log("server is running in the port number:"+PORT ))
