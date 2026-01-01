2ae8288f590a2f5986fec370fc968bf7f55ab69b
aditya_basu123 <adityabasu6204@gmail.com>
2026-01-01 22:43:17 +0530

    message feature added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/send",(re,res)=>{
-  res.send("Send messgage endpoint");
-})
+router.get("/contacts",protectRoute,getAllContacts);
+router.get("/chats",getChatPartners);
+router.get("/:id",getMessagesByUserId);
+
+router.post("/send/:id",sendMessage);
 
 export default router;
